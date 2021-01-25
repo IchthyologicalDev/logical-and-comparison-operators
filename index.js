d40ee287be5c3be05b240ba338d93c8d9b152ff1
Ichthyological Dev <ichthyologicaldev@gmail.com>
2021-01-25 06:00:32 -0800

    template update

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 /*
-Welcome to the js exercise template! This paragraph should be changed for
-each exercise. It is intended to give some high level instruction about the 
-exercise goal. The rest of this multi-line comment should be included in every
-exercise.
+Welcome to the arithmetic operators exercise! You'll get to practice writing functions
+that use some common arithmetic operators. If you get stuck, follow the tips on the 
+webpage.
 
-Look through this file for requirements that start with //REQUIREMENT N:
-You will add your code on the line or lines that start with //TODO:
+Look through this file for requirements that start with REQUIREMENT N:
+Be sure to use the function name and order of parameters specified in the TODOs
 
-EXAMPLE:
-//REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let ichthyologistFocus = null;
+As an example,
+REQUIREMENT 1: My calculator is broken. When I try to do 5 + 5, it returns 55.
 
-You would change the previous line so that it says: 
-let ichthyologistFocus = 'fish';
+TODO: On the following lines, create a function named 'add' that takes in two parameters
+that are either strings or numbers and returns the numeric sum
+
+EXAMPLE CALL
+add('5', '5') returns 10
+//YOUR FUNCTION GOES BELOW THIS LINE
+function add(x,y) {
+    //your logic would go here
+}
+
+This function would not pass, since it clearly doesn't return the numeric sum
 
 You will occasionally see longer comments (like this one) that will include extra
 instruction, helpful hints, best practices, or other content. 
@@ -23,66 +29,119 @@ You should have the index.html file open in your browser as you work through thi
 exercise. If you aren't sure what this means, you should watch the How to Edit Code 
 video: https://youtu.be/vkWDQzl_x3s before continuing.
 
-I hope that you get hooked on coding!
+Have fun casting strings to numbers when necessary!
 */
 
-//REQUIREMENT 1: Do Ichthyologists study fish, birds, or mammals?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let ichthyologistFocus = null;
+/*
+REQUIREMENT 1: My calculator is broken. When I try to do 5 + 5, it returns 55.
 
-//REQUIREMENT 2: What is your name?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let developerName = null;
+TODO: After this comment, create a function named 'add' that takes in two parameters
+that are either strings or numbers and returns the numeric sum
 
-//REQUIREMENT 3: How many oceans are there?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let numberOfOceans = null;
+EXAMPLE CALL
+add('5', '5') returns 10
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
 
-//REQUIREMENT 4: True or false, you are having fun wielding your knowledge of JavaScript data types!
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let havingFun = null;
-
-//REQUIREMENT 5: What is the state fish of Hawaii? (The one that's fun to say!)
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let hawaiianStateFish = null;
-
-//REQUIREMENT 6: How many kilometers long is the Congo River?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let congoLength = null;
-
-//REQUIREMENT 7: True or false, you have caught a fish.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let haveCaughtFish = null;
-
-//REQUIREMENT 8: The largest regular number that JavaScript can reliably handle is 9007199254740991. Make a BigInt that's larger!
-//TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let bigInteger = null;
-
-//REQUIREMENT 9: True or false, JavaScript is rarely used in large tech companies.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let javaScriptIsRarelyUsed = null;
-
-//REQUIREMENT 10: True or false, you can use the BigInt data type with decimals.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let bigIntWorksForDecimals = null;
-
-//REQUIREMENT 11: How would you store the answer to (9007199254740992 + 1) as a BigInt?
-//TODO: On the following line, change null to your answer. Make sure that it's a BigInt!
-let largeNumber = null;
-
-//REQUIREMENT 12: How many species of salmon are native to North America?
-//TODO: On the following line, change null to your answer. Make sure that it's a number!
-let totalNaSalmonSpecies = null;
-
-//REQUIREMENT 13: What is the longest river in the world?
-//TODO: On the following line, change null to your answer. Make sure that it's a string!
-let longestRiver = null;
-
-//REQUIREMENT 14: True or false, the previous requirements covered all possible JavaScript data types.
-//TODO: On the following line, change null to your answer. Make sure that it's a boolean!
-let seenAllTypes = null;
 
 /*
-You've reached the end of this exercise! If you need more practice with data types, you're in luck.
-You'll use these concepts in every subsequent program you write.
+REQUIREMENT 2: In my fishing guide service, I have a unique way of calculating tips. 
+I round every guide's tips down to the nearest 10, and give the excess to the boat cleaner.
+
+TODO: After this comment, create a function named 'roundDownToTens' that takes in a
+positive integer and returns that integer rounded down to the nearest 10
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 3: I took out a couple interest only loans to launch this guide service. I
+think that I'm being overcharged. Help me out! If I just pay the interest amount,
+what should my monthly payments be? 
+
+TODO: After this comment, create a function named 'getMonthlyPayment' that takes
+in principal (number) and annual interest rate (decimal) and returns the monthly interest
+payment
+
+EXAMPLE CALL
+getMonthlyPayment(1000, .06) returns 5, since there would be $60 of interest over the year
+Formula: principal times annual interest rate to find total interest for a year. Divide that by 12 
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 4: Marginal tax rates can be confusing. What percent am I actually paying?
+
+TODO: After this comment, create a function named 'getTaxRate' that takes in income
+(number) and tax paid (number) and returns the average tax rate as a percent
+
+EXAMPLE CALL
+getTaxRate(50000, 7000) returns 14 since 7000/50000 is .14, which we multiply by 100 for the percentage
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 5: I like helping out other small business owners. I am loaning between 
+$500 and $2000 to help someone launch their pizza company. We haven't worked out
+the interest rate yet, but they guaranteed that they will pay it back in full 1 
+year later. I need an IOU message from this person.
+
+TODO: After this comment, create a function named 'makeIOweYou' that takes in
+a loan amount (number) and interest rate (decimal), and returns a message that says
+"I owe you $<amount>", where <amount> represents the total due after a year.
+
+EXAMPLE CALL
+makeIOweYou(750, .01) returns "I owe you $757.5"
+makeIOweYou(1200, .02) returns "I owe you $1224"
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 6: I have 1 bait bucket per client. I want an equal amount of bait in each
+bucket at the start of the day. How many pieces of bait will be left over after 
+placing an equal amount in each bucket?
+
+//TODO: After this comment, create a function named 'getLeftoverBait' that takes
+in the number of pieces of bait (integer) and the number of buckets (integer),
+and returns the number of pieces of bait that would remain (number)
+
+EXAMPLE CALL
+getLeftoverBait(100, 9) returns 1, since the 9 buckets would have 11 pieces each, with 1 leftover
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 7: I get calls when the fishing heats up in certain areas. Can I make it to
+the new location within 30 minutes?
+
+TODO: After this comment, create a function named getDuration that takes in a velocity
+in miles per hour and distance in miles, and returns the number of minutes the trip will take
+
+EXAMPLE CALL
+getDuration(40, 20) returns 30, since it takes 30 minutes to go 20 miles at 40 mph
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+REQUIREMENT 8: Many fish populations are dwindling. Assuming that each fish can produce 5 fish 
+per year, how many fish would we have in the future if fish were immortal? 
+
+TODO: After this comment, create a function named getPopulation that takes in a
+starting population (number) and number of years, and returns the future fish population (number)
+
+EXAMPLE CALL
+getPopulation(5,1) returns 30, since 5 fish each had 5 more fish.
+getPopulation(10,2) returns 360, since 10 fish each had 5, then each of those 60 had 5 more.
+*/
+//YOUR FUNCTION GOES BELOW THIS LINE
+
+
+/*
+You've reached the end of this exercise! If you need more practice with arithmetic operations,
+go try them in your console! Many applications will involve some type of arithmetic.
 */
--- validation.js
+++ validation.js
@@ -3,14 +3,9 @@ WARNING!!!!!
 This file checks your work. If you make changes to this file, then the tests might not work.
 */
 
-/*
-TEMPLATE GUIDE
-Remove this comment after you've made changes to a new exercise.
 
-A recommended pattern is to create functions for each requirement.  
-*/
-
-const UNCHANGED_MESSAGE = 'Did you make a change yet? If so, make sure that you saved your file and refreshed this page!'
+const UNCHANGED_MESSAGE = 'Did you make a change yet? If so, make sure that you saved your file and refreshed this page!';
+const SUCCESS = Symbol('success');
 
 /**
  * Clears all nested content from an element
@@ -75,6 +70,7 @@ const question = (validator) => {
                 if (e.name === 'ReferenceError') {
                     this.message = 'Your code has an error at or before this question. Open up your console to see more information.'
                     this.passed = false;
+                    console.error(e);
                 }
             }
         }
@@ -102,243 +98,136 @@ example of triggering validation.
 ***********************************************************************************
 */
 
+const objectEquals = (actual, expected) => {
+    throw new Error('NOT IMPLEMENTED');
+}
 
 /**
- * A guard clause that returns a message depending on the success or issue.
- * @param {*} variable - The variable being changed for this requirement
- * @param {string} type - Data type that this variable should be
- * @returns {string} - "success" if the type matches, otherwise a message to be displayed to the user
+ * 
+ * @param {*} actual 
+ * @param {*} expected 
+ * @param {string} successMessage 
+ * @returns resultObject
  */
-const getTypeMessage = (variable, type) => {
-    let message = 'success';
-    let actualType = typeof variable;
-    if (variable === null) {
-        message = 'Did you make a change yet? If so, make sure that you saved your file and refreshed this page!'
-    }
-    else if (actualType !== type) {
-        message = `I was expecting a ${type}, but you entered ${actualType === 'undefined' ? actualType : `a ${actualType}`}.`;
-    }
-    return message;
+const equals = (actual, expected, successMessage) => {
+    return typeof expected == 'object' 
+        ? objectEquals(actual, expected)
+        : actual === expected;
 }
 
-const validateIchthyologistFocus = () => {
-    let message = getTypeMessage(ichthyologistFocus, 'string');
-    if (message === 'success') {
-        if(ichthyologistFocus.toLowerCase() == 'fish') {
-            return resultObject(true,'Correct! Ichthyologists study fish');
-        }
-        else {
-            return resultObject(false,`Not quite, Ichthyologists don't study ${ichthyologistFocus}. Do a quick google search and try again!`);
-        }
-    }
-    else {
-        return resultObject(false, message);
-    }
+/**
+ * NOTE: The error message is only accurate when using primitives
+ * @param {function} fn - function under test
+ * @param {* []} given - arguments array
+ * @param {*} expected
+ * @param {string} successMessage 
+ * @returns resultObject
+ */
+const getResultMessage = (fn, given, expected) => {
+    const actual = fn(...given);
+    return equals(actual, expected)
+        ? SUCCESS
+        : `Error when running ${fn.name}(${[...given].toString()}): Expected ${expected}, but got ${actual}.${expected == actual ? ' Check your data type!':''}`;
 }
 
-const validateDeveloperName = () => {
-    let message = getTypeMessage(developerName, 'string');
-    if(message === 'success') {
-        return resultObject(true, `If you say your name is ${developerName}, that's what I'll call you! Have fun practicing data types, ${developerName}!`);
-    } else {
-        return resultObject(false, message);
+const makeCase = (given, expected) => ({given, expected})
+
+const runCases = (fn, testCases, successMessage) => {
+    for(const testCase of testCases) {
+        const {given, expected} = testCase;
+        const result = getResultMessage(fn, given, expected);
+        if(result != SUCCESS) {
+            return resultObject(false, result);
+        }
     }
+    return resultObject(true, successMessage)
 }
 
-const validateNumberOfOceans = () => {
-    let message = getTypeMessage(numberOfOceans, 'number');
-    if (message === 'success') {
-        switch(numberOfOceans) {
-            case 1:
-                return resultObject(true, '1 global ocean? ><> ><> ><> I\'ll accept that.');
-            case 4:
-                return resultObject(true, 'I learned that there were only 4 ocean basins as a kid, but the Southern Ocean is recognized as its own basin. This counts.');
-            case 5:
-                return resultObject(true, 'Nice job!');
-            default:
-                return resultObject(true, `I can't possibly know if the internet lied to you, or if the internet lied to me. I was expecting either 1, 4, or 5 as your answer, not ${numberOfOceans}. Since you put a number here, I'll take it.`)
-        }
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testAdd = () => {
+    const testCases = [
+        makeCase(['5', '5'], 10),
+        makeCase(['-5', '5'], 0),
+        makeCase(['3.1', 3], 6.1),
+        makeCase([1.2, 2.1], 3.3),
+        makeCase([42, '0'], 42)
+    ]
+    return runCases(add, testCases, 'This all adds up to a successful function!');
 }
 
-const validateHavingFun = () => {
-    let message = getTypeMessage(havingFun, 'boolean');
-    if (message === 'success') {
-        message = havingFun ? "Glad that you're enjoying yourself!" : "Sorry that you're not having fun. Leave me a comment on Youtube if you have any suggestions.";
-        return resultObject(true, message);
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testRoundDownToTens = () => {
+    const testCases = [
+        makeCase([10], 10),
+        makeCase([22], 20),
+        makeCase([3], 0),
+    ]
+    return runCases(roundDownToTens, testCases, 'Success! We just round them all down, and just drop the remainder.');
 }
 
-const validateHawaiianStateFish = () => {
-    let message = getTypeMessage(hawaiianStateFish, 'string');
-    if (message === 'success') {
-        let fish = hawaiianStateFish.toLowerCase().replace("'",).replace('ā', 'a');
-        switch (fish) {
-            case 'humuhumunukunukuapuaa':
-            case 'humuhumu':
-                return resultObject(true, "Ae! The Humuhumunukunukuapua'a is Hawaii's state fish. Try saying that 10 times.");
-            case 'reef triggerfish':
-                return resultObject(false, "You're technically right, but Reef Triggerfish isn't as fun to say as its other name. Try again.");
-            default:
-                return resultObject(false, `Mahalo, but no, I was looking for humuhumunukunukuapua'a, not ${hawaiianStateFish}. At least you used a string!`)
-
-        }
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testGetMonthlyPayment = () => {
+    const testCases = [
+        makeCase([1000, .06], 5),
+        makeCase([1000, 0], 0),
+        makeCase([0, 1], 0),
+        makeCase([12, 1], 1),
+    ]
+    return runCases(getMonthlyPayment, testCases, 'Success! Did this question pique your interest?');
 }
 
-const validateCongoLength = () => {
-    let message = getTypeMessage(congoLength, 'number');
-    if (message === 'success') {
-        message = congoLength === 4370 ? `Correct! The Congo River is ${congoLength} km long.` : `Hmmm. I thought it was 4370 km long. If you say it's ${congoLength} km, I'll take your word for it.`;
-        return resultObject(true, message);
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testGetTaxRate = () => {
+    const testCases = [
+        makeCase([1000, 100], 10),
+        makeCase([1000, 0], 0),
+        makeCase([6000, 1500], 25),
+    ]
+    return runCases(getTaxRate, testCases, 'Success! The IRS wants to know if you\'re job hunting.');
 }
 
-const validateHaveCaughtFish = () => {
-    let message = getTypeMessage(haveCaughtFish, 'boolean');
-    if (message === 'success') {
-        message = 'Nothing wrong with that.'
-        if(haveCaughtFish) {
-            message = 'Good for you!'
-        }
-        return resultObject(true, message);
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testMakeIOweYou = () => {
+    const testCases = [
+        makeCase([750, .01], 'I owe you $757.5'),
+        makeCase([1200, .02], 'I owe you $1224'),
+        makeCase([1500, 0], 'I owe you $1500'),
+    ]
+    return runCases(makeIOweYou, testCases, 'I owe you a success message!');
 }
 
-const validateBigInteger = () => {
-    let message = getTypeMessage(bigInteger, 'bigint');
-    if (message === 'success') {
-        if (bigInteger > Number.MAX_SAFE_INTEGER) {
-            message = `${bigInteger} truly is a big integer.`
-        }
-        else {
-            return resultObject(false, `I want a BIG integer. Make sure it's larger than ${Number.MAX_SAFE_INTEGER}n.`);
-        }
-        return resultObject(true, message);
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testGetLeftoverBait = () => {
+    const testCases = [
+        makeCase([100, 9], 1),
+        makeCase([89, 7], 5),
+        makeCase([42, 1], 0),
+    ]
+    return runCases(getLeftoverBait, testCases, 'Remainder of 0 failing test cases');
 }
 
-const validateJavaScriptIsRarelyUsed = () => {
-    let message = getTypeMessage(javaScriptIsRarelyUsed, 'boolean');
-    if (message === 'success') {
-        if(!javaScriptIsRarelyUsed) {
-            return resultObject(true, 'Correct! At the time of writing, JavaScript is by far the most used programming language. Do a search for "does X company use JavaScript?" and look at the results. I\'d bet the answer is yes.');
-        }
-        return resultObject(false, 'Nope. JavaScript is heavily used. Even if you don\'t think it should be.');
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testGetDuration = () => {
+    const testCases = [
+        makeCase([40, 20], 30),
+        makeCase([50, 75], 90),
+        makeCase([1, 10], 600),
+    ]
+    return runCases(getDuration, testCases, 'Success! Let\'s jet');
 }
 
-const validateBigIntWorksForDecimals = () => {
-    let message = getTypeMessage(bigIntWorksForDecimals, 'boolean');
-    if (message === 'success') {
-        if(!bigIntWorksForDecimals) {
-            return resultObject(true, 'Correct! The bigint data type only works for integers. It\'s in the name.');
-        }
-
-        return resultObject(false, 'Nope. Integers refer to non-decimal numbers. That means bigint is only for big integers. If you don\'t believe me, try it out in your console!');
-    }
-    else {
-        return resultObject(false, message);
-    }
-}
-
-const validateLargeNumber = () => {
-    let message = getTypeMessage(largeNumber, 'bigint');
-    if (message === 'success') {
-        if (largeNumber === 9007199254740993n) {
-            return resultObject(true, 'Correct! Did you know that 9007199254740993 can\'t be stored as a regular integer? If you try, it turns back into 9007199254740992.');
-        }
-        return resultObject(false, 'That\'s not how you write 9007199254740993n');
-    }
-    else {
-        return resultObject(false, message);
-    }
-}
-
-const validateTotalNaSalmonSpecies = () => {
-    let message = getTypeMessage(totalNaSalmonSpecies, 'number');
-    if (message === 'success') {
-        if(totalNaSalmonSpecies === 6) {
-            return resultObject(true, 'Correct! There are 6. My favorite is the Sockeye/Red salmon.');
-        }
-        if(totalNaSalmonSpecies < 6) {
-            message = `There are more than ${totalNaSalmonSpecies} species native to North America. Did you include both coasts?`;
-        }
-        else {
-            message = `Reel it back a little. There aren't quite ${totalNaSalmonSpecies} salmon species that are native to North America.`;
-        }
-        return resultObject(false, message);
-    }
-    else {
-        return resultObject(false, message);
-    }
-}
-
-const validateLongestRiver = () => {
-    let message = getTypeMessage(longestRiver, 'string');
-    if (message === 'success') {
-        if(longestRiver.toLowerCase() === 'nile') {
-            return resultObject(true, 'Correct! Not only is da Nile the longest river in the world, but it\'s a mindset that developers frequently have to combat!');
-        }
-        else {
-            return resultObject(false, `If you're in denial that it's not the ${longestRiver}, then you should know what to guess next.`);
-        }
-    }
-    else {
-        return resultObject(false, message);
-    }
-}
-
-const validateSeenAllTypes = () => {
-    let message = getTypeMessage(seenAllTypes, 'boolean');
-    if (message === 'success') {
-        if(!seenAllTypes) {
-            return resultObject(true, 'Correct! You\'re missing out on Symbol, undefined, null, and the endless flexibility of objects (which null happens to be)');
-        }
-        return resultObject(false, 'Even if you\'ve seen all types, they weren\'t covered in this exercise.');
-    }
-    else {
-        return resultObject(false, message);
-    }
+const testGetPopulation = () => {
+    const testCases = [
+        makeCase([5, 1], 30),
+        makeCase([10, 2], 360),
+        makeCase([7, 0], 7),
+    ]
+    return runCases(getPopulation, testCases, '"So many friends, SO MANY FISH!" - Lucy Cousins');
 }
 
 //NOTE: The order of questions in this array must match the order of requirements on the DOM and in the index.js file.
 const questions = [
-    question(validateIchthyologistFocus),
-    question(validateDeveloperName),
-    question(validateNumberOfOceans),
-    question(validateHavingFun),
-    question(validateHawaiianStateFish),
-    question(validateCongoLength),
-    question(validateHaveCaughtFish),
-    question(validateBigInteger),
-    question(validateJavaScriptIsRarelyUsed),
-    question(validateBigIntWorksForDecimals),
-    question(validateLargeNumber),
-    question(validateTotalNaSalmonSpecies),
-    question(validateLongestRiver),
-    question(validateSeenAllTypes),
+    question(testAdd),
+    question(testRoundDownToTens),
+    question(testGetMonthlyPayment),
+    question(testGetTaxRate),
+    question(testMakeIOweYou),
+    question(testGetLeftoverBait),
+    question(testGetDuration),
+    question(testGetPopulation)
 ]
 
 runValidation(questions);
